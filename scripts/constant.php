85609fbe8cea3b9f836ab73657e2aeb52bbfdad2
Abhimanyu Kachhwaha <abhimanyukachhwaha1@arspl.com>
2021-02-10 12:31:31 +0530

    mailer

--- scripts/constant.php
+++
@@ -1,5 +0,0 @@
-<?php
-//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
-define('SITE_KEY',"6LevLvYUAAAAAMNLijXe99Hqy-rKEr8Tdey7ZmWD"); 
-define('SECRET_KEY',"6LevLvYUAAAAANEMrh-pnxUj5c430_h1UIj6bjxN");
-?>
