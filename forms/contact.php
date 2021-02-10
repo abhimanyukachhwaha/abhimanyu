8d135b538c66f7d60b11a87ed199c5b5ab995563
Abhimanyu Kachhwaha <abhimanyukachhwaha1@arspl.com>
2021-02-10 12:44:57 +0530

    dd

--- forms/contact.php
+++ forms/contact.php
@@ -15,7 +15,7 @@
 require_once '../mailer/class.phpmailer.php'; 
 
 if(isset($_POST) && !empty($_POST)){
-	//echo "<pre>"; print_r($_POST);
+	echo "<pre>"; print_r($_POST);
     $name         = $_POST['name'];
     $email         = $_POST['email'];
     $subject        = $_POST['subject'];
