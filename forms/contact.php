03d0fa4f35d82ea8fe3aa2afb6d483c90d4188bd
Abhimanyu Kachhwaha <abhimanyukachhwaha1@arspl.com>
2021-02-10 12:37:58 +0530

    mailer

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,92 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'abhimanyukachhwaha@rathi.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+   if($_POST)
+{
+//require('constant.php');
+    
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
-  $contact->smtp = array(
-    'host' => 'mail.smtp2go.com',
-    'username' => 'vijaybissa@rathi.com',
-    'password' => 'Vijay@123456',
-    'port' => '2525'
-  );
-  
+    
+   
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+
+require_once '../mailer/class.phpmailer.php'; 
+
+if(isset($_POST) && !empty($_POST)){
+	//echo "<pre>"; print_r($_POST);
+    $name         = $_POST['name'];
+    $email         = $_POST['email'];
+    $subject        = $_POST['subject'];
+	$message     = $_POST['message'];
+    
+    $html = "
+                
+				    <P>Name: ".$name." </p>
+                    <p>Email: ".$email."</p>
+                    <th>Subject: ".$subject."</p>
+                    <p>Message: ".$message."</p>
+                
+				    
+                
+            
+            ";
+$mail = new PHPMailer(true); 
+$mail->IsSMTP();
+$mail->Mailer = "smtp";
+    
+    try { 
+	
+      //$mail->SMTPDebug = 2;									 
+         //$mail->IsSMTP(); 
+		 //$mail->isHTML(true);
+		 //$mail->Host       = "listserv.rathi.com";      
+		 //$mail->Port       = 25;             
+		 //$mail->AddAddress($email1);
+		 //$mail->Username   ="info@rathi.com";  
+		//$mail->Password   ="your_gmail_password";
+            	
+	
+$mail->isSMTP();
+$mail->Host = 'listserv.rathi.com';
+$mail->SMTPAuth = false;
+$mail->SMTPAutoTLS = false; 
+$mail->Port = 25; 
+$mail->Username   ="abhiartsstudios@gmail.com";  
+ //$mail->Password   ="your_gmail_password";
+ 
+    
+        $mail->setFrom( $email,'Abhi Studios');		 
+        $mail->AddReplyTo("abhiartsstudios@gmail.com","abhiartstudios@gmail.com");
+	    $mail->AddCC("vijaybissa@rathi.com"); 
+        // $mail->addAddress('receiver2@gfg.com', 'Name'); 
+        
+        $mail->isHTML(true);								 
+        $mail->Subject = "Query from abhistudios by $name"; 
+        $mail->Body = $html; 
+        $mail->send();
+        $output = array(
+            'status' => 1,
+        ); 
+		//print_r('here');
+    } catch (Exception $e) { 
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"; //die;
+        $output = array(
+            'status' => 0,
+        );
+    } 
+    
+}else{
+    $output = array(
+        'status' => 0,
+    );
+}
+echo json_encode($output);die; 
+
+
+
+
+} 
+
 ?>
